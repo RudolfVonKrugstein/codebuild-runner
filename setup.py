6ad5dbce4304690a3cdad745228b0383afdf8f5d
Nathan <nathan.huesken@posteo.de>
2018-01-16 16:38:29 +0100

    Try different way of specifying files

--- setup.py
+++ setup.py
@@ -103,8 +103,9 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(include=['.'],exclude=['contrib', 'docs', 'tests']),  # Required
-
+    #packages=find_packages(include=['.'],exclude=['contrib', 'docs', 'tests']),  # Required
+    py_modules=["main","zip"]
+    
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
